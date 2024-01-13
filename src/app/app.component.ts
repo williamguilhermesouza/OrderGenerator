c3403027634560d2a0f15d622ce2a18bb274922b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2024-01-13 12:11:34 -0300

    finished first styling, working on service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'OrderGenerator';
+  isBuyOrder: boolean = true;
+
+  orderForm = this.formBuilder.group({
+    stockName: ['PETR4', Validators.required],
+    quantity: ['', Validators.required],
+    price: ['', Validators.required],
+  });
+
+  constructor(private formBuilder : FormBuilder) {}
+
+  onBuyClickHandler() {
+    this.isBuyOrder = true;
+  }
 
   onSellClickHandler() {
-    console.log('clicked!');
+    this.isBuyOrder = false;
+  }
+
+  onSubmit() {
+    alert(`ativo: ${this.orderForm.value.stockName}, quantidade: ${this.orderForm.value.quantity}, preço: ${this.orderForm.value.price}`);
   }
 }
